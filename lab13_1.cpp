aa0ba92e21dfdc740faca42585172585ee8a5103
Nongdada <ronnachai_si@cmu.ac.th>
2023-01-10 15:03:29 +0700

    "complete"

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    if( x == 0) {return x = 0;}
+    else if (x == 1){return x = 1;}
+    else if(x > 1){ return fibonacci(x-1) + fibonacci(x-2);}
+    else {
+
+    }
+    return x;
 }
